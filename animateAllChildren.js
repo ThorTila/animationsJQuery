15d63164b5d10ae579a2cab58238d83aafe956a5
Thortilla <33997787+ThorTila@users.noreply.github.com>
2018-06-11 18:56:58 +0200

    rewrite to use less code

--- animateAllChildren.js
+++ animateAllChildren.js
@@ -5,86 +5,36 @@
 
 (function($) {
   /**
-   * Animations types
-   *
-   * @enum {string}
-   * @readonly
-   */
-  var animations = Object.freeze({
-    FADETOGGLE: 'fadetoggle',
-    FADEOUT: 'fadeout',
-    FADEIN: 'fadein',
-    SLIDETOGGLE: 'slidetoggle',
-    SLIDEUP: 'slideup',
-    SLIDEDOWN: 'slidedown'
-  });
-  /**
    * Function to trigger chosen jQuery animation.
    *
    * @name animateAllChildren
    * @param {animations} type Type of animation you want to trigger
-   * @param {number} [duration=5000] Duration of animation (default: 5000)
+   * @param {number|object} [options=400] Duration of animation or object with options. {@link http://api.jquery.com/fadeout/#fadeOut-options See more} (default: 400)
    * @param {number} [delay=200] Delay between animation starts on every child (default: 200)
    * @returns Element on which it was called
    * @throws {SyntaxError} When wrong animation type is passed
    * @example
    * $('ul').animateAllChildren('fadeOut');
-   * //trigger animation 'fadeOut' on all ul`s children with duration 5000ms and delay between them 200ms
+   * //trigger animation 'fadeOut' on all ul`s children with duration 400ms and delay between them 200ms
    * @example
-   * $('ul').animateAllChildren('slideUP', 500, 100);
+   * $('ul').animateAllChildren('slideUp', 500, 100);
    * //trigger animation 'slideUp' on all ul`s children with duration 500ms and delay between them 100ms
+   * @example
+   * $('ul').animateAllChildren('slideDown', { duration: 200, easing: "linear" }, 100);
+   * //trigger animation 'slideDown' on all ul`s children with duration 200ms, linear easing function and delay between them 100ms
    */
-  $.fn.animateAllChildren = function(type, duration, delay) {
-    if (!type) throw new SyntaxError('No arg passed');
-    var elements = this.children(),
-      duration = duration || 5000,
+  $.fn.animateAllChildren = function(type, options, delay) {
+    if (!$(this)[type]) throw SyntaxError('Wrong animation type passed.');
+    var options = options || {},
       delay = delay || 200;
-    switch (type.toLowerCase()) {
-      case animations.FADETOGGLE:
-        elements.each(function(i) {
-          $(this)
-            .delay(delay * i)
-            .fadeToggle({ queue: true, duration: duration });
-        });
-        break;
-      case animations.FADEOUT:
-        elements.each(function(i) {
-          $(this)
-            .delay(delay * i)
-            .fadeOut({ queue: true, duration: duration });
-        });
-        break;
-      case animations.FADEIN:
-        elements.each(function(i) {
-          $(this)
-            .delay(delay * i)
-            .fadeIn({ queue: true, duration: duration });
-        });
-        break;
-      case animations.SLIDETOGGLE:
-        elements.each(function(i) {
-          $(this)
-            .delay(delay * i)
-            .slideToggle({ queue: true, duration: duration });
-        });
-        break;
-      case animations.SLIDEUP:
-        elements.each(function(i) {
-          $(this)
-            .delay(delay * i)
-            .slideUp({ queue: true, duration: duration });
-        });
-        break;
-      case animations.SLIDEDOWN:
-        elements.each(function(i) {
-          $(this)
-            .delay(delay * i)
-            .slideDown({ queue: true, duration: duration });
-        });
-        break;
-      default:
-        throw new SyntaxError('Wrong animation type passed.');
-    }
+
+    var elements = this.children();
+
+    $(elements).each(function(i) {
+      $(this)
+        .delay(delay * i)
+        [type](options);
+    });
     return this;
   };
 })(jQuery);
